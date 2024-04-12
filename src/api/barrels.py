7524f4958081c9d78f0c2d3bf488b2f3f5001508
JefferNg <jefferng11@gmail.com>
2024-04-12 14:02:35 -0700

    updated barrels and bottler

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -28,7 +28,8 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
         for row in result:
             if row.num_green_potions < 10:
                 result = connection.execute(sqlalchemy.text
-                (f"UPDATE global_inventory SET num_green_ml = {(barrels_delivered[0].ml_per_barrel + row.num_green_ml)*barrels_delivered[0].quantity}"))
+                (f"UPDATE global_inventory SET num_green_ml = {
+                    (barrels_delivered[0].ml_per_barrel + row.num_green_ml)*barrels_delivered[0].quantity}"))
 
     return "OK"
 
@@ -39,13 +40,22 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print(wholesale_catalog)
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-    num = 0
+    gold = 0
     for row in result:
-        num = row.num_green_potions
-    return [
-        {
-            "sku": "SMALL_GREEN_BARREL",
-            "quantity": num,
-        }
-    ]
+        gold = row.gold
+
+    num = 0
+    barrel_plan = []
+    for barrel in wholesale_catalog:
+        # cycle through different barrels
+        if (barrel.sku == "SMALL_GREEN_BARREL"):
+            if wholesale_catalog[0].price <= gold:
+                barrel_plan.append(        {
+                    "sku": "SMALL_GREEN_BARREL",
+                    "quantity": 1
+                })
+
+
+    return barrel_plan
+
 
