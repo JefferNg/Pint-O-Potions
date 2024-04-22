4ae65d07acdf605800ccc8d499e5ee94ade06231
JefferNg <jefferng11@gmail.com>
2024-04-21 19:18:40 -0700

    updated logic in bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -21,33 +21,32 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-        potion_mix = potions_delivered[0].potion_type
         for row in result:
             for potions in potions_delivered:
-                if potion_mix[0] == 100:
+                if potions.potion_type[0] == 100:
                     # update red bottle and ml count
                     result = connection.execute(sqlalchemy.text
                     (f"UPDATE global_inventory SET num_red_potions = {potions.quantity + row.num_red_potions}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_red_ml = {row.num_red_ml - potion_mix[0]}"))
-                elif potion_mix[1] == 100:
+                    (f"UPDATE global_inventory SET num_red_ml = {row.num_red_ml - potions.potion_type[0]}"))
+                elif potions.potion_type[1] == 100:
                     # update green bottle and ml count
                     result = connection.execute(sqlalchemy.text
                     (f"UPDATE global_inventory SET num_green_potions = {potions.quantity + row.num_green_potions}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_green_ml = {row.num_green_ml - potion_mix[1]}"))
-                elif potion_mix[2] == 100:
+                    (f"UPDATE global_inventory SET num_green_ml = {row.num_green_ml - potions.potion_type[1]}"))
+                elif potions.potion_type[2] == 100:
                     # update blue bottle and ml count
                     result = connection.execute(sqlalchemy.text
                     (f"UPDATE global_inventory SET num_blue_potions = {potions.quantity + row.num_blue_potions}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_blue_ml = {row.num_blue_ml - potion_mix[2]}"))
-                elif potion_mix[3] == 100:
+                    (f"UPDATE global_inventory SET num_blue_ml = {row.num_blue_ml - potions.potion_type[2]}"))
+                elif potions.potion_type[3] == 100:
                     # update dark bottle and ml count
                     result = connection.execute(sqlalchemy.text
                     (f"UPDATE global_inventory SET num_dark_potions = {potions.quantity + row.num_dark_potions}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_dark_ml = {row.num_dark_ml - potion_mix[3]}"))
+                    (f"UPDATE global_inventory SET num_dark_ml = {row.num_dark_ml - potions.potion_type[3]}"))
                 else:
                     raise Exception("No Potion Mixed")
 
