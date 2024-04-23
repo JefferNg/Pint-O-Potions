a14fb002e0d9553e12d2672ba38c1f9abcb9221d
JefferNg <jefferng11@gmail.com>
2024-04-23 01:01:51 -0700

    implemented potion table

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -20,33 +20,101 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     """ """
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-        for row in result:
+        inventory = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+        potion_inventory = connection.execute(sqlalchemy.text("SELECT * FROM potion_inventory"))
+        red_potions = 0
+        green_potions = 0
+        blue_potions = 0
+        dark_potions = 0
+        purple_potions = 0
+        forest_potions = 0
+        navy_potions = 0
+        maroon_potions = 0
+        unmarked_potions = 0
+        for potion in potion_inventory:
+            if potion.sku == "RED_POTION":
+                red_potions = potion.quantity
+            elif potion.sku == "GREEN_POTION":
+                green_potions = potion.quantity
+            elif potion.sku == "BLUE_POTION":
+                blue_potions = potion.quantity
+            elif potion.sku == "DARK_POTION":
+                dark_potions = potion.quantity
+            elif potion.sku == "PURPLE_POTION":
+                purple_potions = potion.quantity
+            elif potion.sku == "FOREST_POTION":
+                forest_potions = potion.quantity
+            elif potion.sku == "NAVY_POTION":
+                navy_potions = potion.quantity
+            elif potion.sku == "MAROON_POTION":
+                maroon_potions = potion.quantity
+            elif potion.sku == "UNMARKED_POTION":
+                unmarked_potions = potion.quantity
+        
+        # for potions in potions_delivered:
+        #     if potions.potion_type == [100,0,0,0]:
+        #         connection.execute(sqlalchemy.text
+        #             (f"UPDATE potion_inventory SET quantity = {potions.quantity + red_potions}, price = 70 WHERE type = array{potions.potion_type}"))
+        for row in inventory:
+            red_ml = row.num_red_ml
+            green_ml = row.num_green_ml
+            blue_ml = row.num_blue_ml
+            dark_ml = row.num_dark_ml
             for potions in potions_delivered:
-                if potions.potion_type[0] == 100:
+                if potions.potion_type == [100,0,0,0]:
                     # update red bottle and ml count
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_red_potions = {potions.quantity + row.num_red_potions}"))
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + red_potions} WHERE type = array{potions.potion_type}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_red_ml = {row.num_red_ml - potions.potion_type[0]}"))
-                elif potions.potion_type[1] == 100:
+                    (f"UPDATE global_inventory SET num_red_ml = {red_ml - potions.potion_type[0]}"))
+                elif potions.potion_type == [0,100,0,0]:
                     # update green bottle and ml count
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_green_potions = {potions.quantity + row.num_green_potions}"))
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + green_potions} WHERE type = array{potions.potion_type}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_green_ml = {row.num_green_ml - potions.potion_type[1]}"))
-                elif potions.potion_type[2] == 100:
+                    (f"UPDATE global_inventory SET num_green_ml = {green_ml - potions.potion_type[1]}"))
+                elif potions.potion_type == [0,0,100,0]:
                     # update blue bottle and ml count
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_blue_potions = {potions.quantity + row.num_blue_potions}"))
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + blue_potions} WHERE type = array{potions.potion_type}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_blue_ml = {row.num_blue_ml - potions.potion_type[2]}"))
-                elif potions.potion_type[3] == 100:
+                    (f"UPDATE global_inventory SET num_blue_ml = {blue_ml - potions.potion_type[2]}"))
+                elif potions.potion_type == [0,0,0,100]:
                     # update dark bottle and ml count
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_dark_potions = {potions.quantity + row.num_dark_potions}"))
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + dark_potions} WHERE type = array{potions.potion_type}"))
                     result = connection.execute(sqlalchemy.text
-                    (f"UPDATE global_inventory SET num_dark_ml = {row.num_dark_ml - potions.potion_type[3]}"))
+                    (f"UPDATE global_inventory SET num_dark_ml = {dark_ml - potions.potion_type[3]}"))
+                elif potions.potion_type == [50,0,50,0]:
+                    # update purple bottle and ml count
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + purple_potions} WHERE type = array{potions.potion_type}"))
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE global_inventory SET num_red_ml = {red_ml - potions.potion_type[0]}, num_blue_ml = {blue_ml - potions.potion_type[2]}"))
+                elif potions.potion_type == [0,50,0,50]:
+                    # update forest bottle and ml count
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + forest_potions} WHERE type = array{potions.potion_type}"))
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE global_inventory SET num_green_ml = {green_ml - potions.potion_type[1]}, num_dark_ml = {dark_ml - potions.potion_type[3]}"))
+                elif potions.potion_type == [0,0,50,50]:
+                    # update navy bottle and ml count
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + navy_potions} WHERE type = array{potions.potion_type}"))
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE global_inventory SET num_blue_ml = {blue_ml - potions.potion_type[2]}, num_dark_ml = {dark_ml - potions.potion_type[3]}"))
+                elif potions.potion_type == [50,0,0,50]:
+                    # update maroon bottle and ml count
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + maroon_potions} WHERE type = array{potions.potion_type}"))
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE global_inventory SET num_red_ml = {red_ml - potions.potion_type[0]}, num_dark_ml = {dark_ml - potions.potion_type[3]}"))
+                elif potions.potion_type == [25,25,25,25]:
+                    # update unmarked bottle and ml count
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE potion_inventory SET quantity = {potions.quantity + unmarked_potions} WHERE type = array{potions.potion_type}"))
+                    result = connection.execute(sqlalchemy.text
+                    (f"UPDATE global_inventory SET num_red_ml = {red_ml - potions.potion_type[0]}, num_green_ml = {green_ml - potions.potion_type[1]}, num_blue_ml = {blue_ml - potions.potion_type[2]}, num_dark_ml = {dark_ml - potions.potion_type[3]}"))
                 else:
                     raise Exception("No Potion Mixed")
 
@@ -106,40 +174,40 @@ def get_bottle_plan():
             "quantity": 1,
         })
 
-    # if red_ml >= 50 and blue_ml >= 50:
-    #     # bottle purple
-    #     bottle_plan.append({
-    #         "potion_type": [50, 0, 50, 0],
-    #         "quantity": 1,
-    #     })
+    if red_ml >= 50 and blue_ml >= 50:
+        # bottle purple
+        bottle_plan.append({
+            "potion_type": [50, 0, 50, 0],
+            "quantity": 1,
+        })
 
-    # if blue_ml >= 50 and dark_ml >= 50:
-    #     # bottle navy
-    #     bottle_plan.append({
-    #         "potion_type": [0, 0, 50, 50],
-    #         "quantity": 1,
-    #     })
+    if blue_ml >= 50 and dark_ml >= 50:
+        # bottle navy
+        bottle_plan.append({
+            "potion_type": [0, 0, 50, 50],
+            "quantity": 1,
+        })
     
-    # if red_ml >= 50 and dark_ml >= 50:
-    #     # bottle maroon
-    #     bottle_plan.append({
-    #         "potion_type": [50, 0, 0, 50],
-    #         "quantity": 1,
-    #     })
+    if red_ml >= 50 and dark_ml >= 50:
+        # bottle maroon
+        bottle_plan.append({
+            "potion_type": [50, 0, 0, 50],
+            "quantity": 1,
+        })
 
-    # if green_ml >= 50 and dark_ml >= 50:
-    #     # bottle forest
-    #     bottle_plan.append({
-    #         "potion_type": [0, 50, 0, 50],
-    #         "quantity": 1,
-    #     })
+    if green_ml >= 50 and dark_ml >= 50:
+        # bottle forest
+        bottle_plan.append({
+            "potion_type": [0, 50, 0, 50],
+            "quantity": 1,
+        })
 
-    # if red_ml >= 25 and green_ml >= 25 and blue_ml >= 25 and dark_ml >= 25:
-    #     # bottle useless
-    #     bottle_plan.append({
-    #         "potion_type": [25, 25, 25, 25],
-    #         "quantity": 1,
-    #     })
+    if red_ml >= 25 and green_ml >= 25 and blue_ml >= 25 and dark_ml >= 25:
+        # bottle useless
+        bottle_plan.append({
+            "potion_type": [25, 25, 25, 25],
+            "quantity": 1,
+        })
     
     if len(bottle_plan) > 1:
         # potions were mixed
--- src/api/catalog.py
+++ src/api/catalog.py
@@ -11,48 +11,80 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-        green_potions = 0
-        red_potions = 0
-        blue_potions = 0
-        dark_potions = 0
-        for row in result:
-            green_potions = row.num_green_potions
-            red_potions = row.num_red_potions
-            blue_potions = row.num_blue_potions
-            dark_potions = row.num_dark_potions
-    catalog = []
-    if red_potions > 0:
-        catalog.append(            {
-                "sku": "RED_POTION",
-                "name": "Health",
-                "quantity": red_potions,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            })
-    if green_potions > 0:
-        catalog.append(            {
-                "sku": "GREEN_POTION",
-                "name": "Leap",
-                "quantity": green_potions,
-                "price": 50,
-                "potion_type": [0, 100, 0, 0],
-            })
-    if blue_potions > 0:
-        catalog.append(            {
-                "sku": "BLUE_POTION",
-                "name": "Swiftness",
-                "quantity": blue_potions,
-                "price": 50,
-                "potion_type": [0, 0, 100, 0],
-            })
-    if dark_potions > 0:
-        catalog.append(            {
-                "sku": "DARK_POTION",
-                "name": "Invisibility",
-                "quantity": dark_potions,
-                "price": 50,
-                "potion_type": [0, 0, 0, 100],
-            })
+        result = connection.execute(sqlalchemy.text("SELECT * FROM potion_inventory"))
+        catalog = []
+        for row in result: 
+            if row.sku == "RED_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Health",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "GREEN_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Leap",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "BLUE_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Swiftness I",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "DARK_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Invisibility",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "PURPLE_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Teleportation",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "FOREST_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Swiftness II",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "NAVY_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Night Vision",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "MAROON_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "Regeneration",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
+            if row.sku == "UNMARKED_POTION" and row.quantity > 0:
+                catalog.append(            {
+                        "sku": row.sku,
+                        "name": "???",
+                        "quantity": row.quantity,
+                        "price": row.price,
+                        "potion_type": row.type,
+                    })
 
     return catalog
