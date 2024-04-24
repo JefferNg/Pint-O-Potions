707632dce1dd6000ced51114c82194763c81c0ba
JefferNg <jefferng11@gmail.com>
2024-04-23 21:24:37 -0700

    implemented cart and cart items table

--- src/api/carts.py
+++ src/api/carts.py
@@ -84,35 +84,40 @@ def post_visits(visit_id: int, customers: list[Customer]):
     
     return "OK"
 
-class Cart:
-    def __init__(self, cart_id, customer, potion_quantity):
-        self.cart_id = cart_id
-        self.customer = customer
-        self.potion_quantity = potion_quantity
-
-cart_dict = {
-    "cart": []
-}
-
-
-cart_num = 0
 
 @router.post("/")
 def create_cart(new_cart: Customer):
     """ """
-    global cart_num
-    cart_num += 1
-    cart = Cart(cart_num, new_cart, {})
-    cart_list = cart_dict.get("cart")
-    cart_list.append(cart)
-    cart_dict.update({"cart": cart_list})
     print(f"Cart created by: {new_cart}")
+
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text
+        ("INSERT INTO carts (customer) VALUES (:customer)"), 
+        [{"customer": new_cart.customer_name + " " + new_cart.character_class + " " + str(new_cart.level)}])
+        result = connection.execute(sqlalchemy.text("SELECT * FROM carts"))
+        cart_num = 0
+        for row in result:
+            if new_cart.customer_name in row.customer:
+                cart_num = row.id
+
     return {"cart_id": cart_num}
 
 
 class CartItem(BaseModel):
     quantity: int
 
+# convert sku to id
+sku_values = {
+    "RED_POTION": 1,
+    "GREEN_POTION": 2,
+    "BLUE_POTION" : 3,
+    "DARK_POTION": 4,
+    "PURPLE_POTION": 5,
+    "FOREST_POTION": 6,
+    "NAVY_POTION": 7,
+    "MAROON_POTION": 8,
+    "UNMARKED_POTION": 9
+}
 
 @router.post("/{cart_id}/items/{item_sku}")
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
@@ -120,13 +125,17 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     print(f"A customer wants to buy {cart_item.quantity, item_sku}")
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM potion_inventory"))
+        carts = connection.execute(sqlalchemy.text("SELECT * FROM carts"))
         
-        for cart in cart_dict.get("cart"):
-            if cart.cart_id == cart_id:
+        for cart in carts:
+            if cart.id == cart_id:
                 for row in result:
                     if item_sku == row.sku:
                         if row.quantity - cart_item.quantity > 0:
-                            cart.potion_quantity.update({item_sku: cart_item.quantity})
+                            # add potion to cart
+                            connection.execute(sqlalchemy.text
+                            ("INSERT INTO cart_items (cart_id, item_sku, quantity) VALUES (:cart_id, :item_sku, :quantity)"), 
+                            [{"cart_id": cart_id, "item_sku": sku_values.get(item_sku), "quantity": cart_item.quantity}])
                             print(f"item {item_sku, cart_item} added to cart id {cart_id}")
     return "OK"
 
@@ -137,16 +146,37 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
+    print(f"payment: {cart_checkout.payment}")
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM potion_inventory"))
+        carts = connection.execute(sqlalchemy.text("SELECT * FROM carts"))
         total = 0
         gold = 0
-        for cart in cart_dict.get("cart"):
-            if cart.cart_id == cart_id:
+        for cart in carts:
+            if cart.id == cart_id:
+                items = connection.execute(sqlalchemy.text
+                ("SELECT * FROM cart_items WHERE cart_id = :id"), [{"id": cart_id}])
+                list_of_items = {}
+                for item in items:
+                    # add items in cart to local dictionary
+                    if item.item_sku in list_of_items:
+                        # update quantity if item in dictionary
+                        list_of_items.update({item.item_sku: list_of_items.get(item.item_sku) + item.quantity})
+                    else:
+                        # add new item to dictionary
+                        list_of_items[item.item_sku] = item.quantity
+                    # delete item from cart_item table once processed
+                    connection.execute(sqlalchemy.text
+                    ("DELETE FROM cart_items WHERE item_sku = :sku"), [{"sku": item.item_sku}])
+                
                 for row in result:
-                    if row.sku in cart.potion_quantity.keys():
-                        total += cart.potion_quantity.get(row.sku)
-                        connection.execute(sqlalchemy.text(f"UPDATE potion_inventory SET quantity = {row.quantity - cart.potion_quantity.get(row.sku)} WHERE sku = '{row.sku}'"))
-                        gold += row.price * cart.potion_quantity.get(row.sku)
-        connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = {gold}"))
+                    if row.id in list_of_items:
+                        # potion item in cart
+                        total += list_of_items.get(row.id)
+                        connection.execute(sqlalchemy.text
+                        ("UPDATE potion_inventory SET quantity = :total WHERE sku = :sku"), 
+                        [{"total": row.quantity - list_of_items.get(row.id), "sku": row.sku}])
+                        gold += row.price * list_of_items.get(row.id)
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = :gold"), [{"gold": gold}])
+        connection.execute(sqlalchemy.text("DELETE FROM carts WHERE id = :id"), [{"id": cart_id}])
     return {"total_potions_bought": total, "total_gold_paid": gold}
