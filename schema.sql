d2668596d3141a531f280a6acda193b84d5c6433
JefferNg <jefferng11@gmail.com>
2024-04-25 22:49:07 -0700

    added schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+INSERT INTO potion_inventory
+(sku, type, quantity, price)
+VALUES
+("RED_POTION", [100,0,0,0], 0, 50),
+("GREEN_POTION", [0,100,0,0], 0, 50),
+("BLUE_POTION", [0,0,100,0], 0, 55),
+("DARK_POTION", [0,0,0,100], 0, 80),
+("PURPLE_POTION", [50,0,50,0], 0, 60),
+("FOREST_POTION", [0,50,0,50], 0, 70),
+("NAVY_POTION", [0,0,50,50], 0, 70),
+("MAROON_POTION", [50,0,0,50], 0, 70),
+("UNMARKED_POTION", [25,25,25,25], 0, 85)
